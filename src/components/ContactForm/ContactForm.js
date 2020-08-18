3012d42b149928b0c5efbfd1aabecc9ab0296d19
Joshua Page <joshuapage.contact@gmail.com>
2020-08-18 15:42:16 +0100

    meta tags, title and redeploy

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,13 +5,13 @@ import './ContactForm.css'
 export default function App() {
     return (
         <form className="form">
-            <label for="name" className="label">Name</label>
+            <label htmlFor="name" className="label">Name</label>
             <input type="text" name="name" placeholder="Your Name" required />
 
-            <label for="email" className="label">Email</label>
+            <label htmlFor="email" className="label">Email</label>
             <input type="email" name="email" placeholder="Your Email" required />
 
-            <label for="message" className="label">Message</label>
+            <label htmlFor="message" className="label">Message</label>
             <textarea name="message" placeholder="Your message to us" required />
 
             <input type="submit" />
